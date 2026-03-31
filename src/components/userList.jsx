9d26da4d0d3efd71b55a8ba031ae732f0225f957
FatimaPaul <fatimapauly91@gmail.com>
2026-03-31 01:24:18 -0700

    changes in the first button of feat/btn-delete

--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -20,7 +20,7 @@ export default function UserList() {
   return (
     <>
       <button onClick={handleEnterUsers}>Enter Users</button>
-      <button>Set New User</button>
+      <button>Set Employees Delete</button>
       <ol>
         {users.map((user) => (
           <li key={user.id}>{user.firstName}</li>
